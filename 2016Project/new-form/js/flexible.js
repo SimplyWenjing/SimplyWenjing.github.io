779b3311b1f9f1e2358f1a4a37430dcebc070f6d
SimplyWenjing <598228662@qq.com>
2016-04-26 16:30:17 +0800

    Update flexible.js

--- 2016Project/new-form/js/flexible.js
+++ 2016Project/new-form/js/flexible.js
@@ -11,27 +11,27 @@
     if (metaEl) {
         console.warn('将根据已有的meta标签来设置缩放比例');
         var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
-        // if (match) {
-        //     scale = parseFloat(match[1]);
-        //     dpr = parseInt(1 / scale);
-        // }
         if (match) {
-            var isIPhone = win.navigator.appVersion.match(/iphone/gi);
-            var devicePixelRatio = win.devicePixelRatio;
-            if (isIPhone) {
-                // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-                if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
-                    dpr = 3;
-                } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
-                    dpr = 2;
-                } else {
-                    dpr = 1;
-                }
-            }else {
-                scale = parseFloat(match[1]);
-                dpr = parseInt(1 / scale);
-            }
-            scale = 1 / dpr ;
+            scale = parseFloat(match[1]);
+            dpr = parseInt(1 / scale);
+        }
+        // if (match) {
+        //     var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+        //     var devicePixelRatio = win.devicePixelRatio;
+        //     if (isIPhone) {
+        //         // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+        //         if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
+        //             dpr = 3;
+        //         } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+        //             dpr = 2;
+        //         } else {
+        //             dpr = 1;
+        //         }
+        //     }else {
+        //         scale = parseFloat(match[1]);
+        //         dpr = parseInt(1 / scale);
+        //     }
+        //     scale = 1 / dpr ;
         }
     } else if (flexibleEl) {
         var content = flexibleEl.getAttribute('content');
